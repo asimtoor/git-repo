36ffc501b0c9c3acb5ddaebff95b119ab762fc92
atoor <asimtoor@gmail.com>
2017-05-11 18:08:24 -0400

    Added new class

---
+++ git-in-eclipse/src/com/git/exercise/MathOperations.java
@@ -0,0 +1,20 @@
+package com.git.exercise;
+
+public class MathOperations {
+
+	public static void main(String[] args) {
+		
+		int n = 23;
+		int m = 77;
+		
+		System.out.println(Sum(n, m));
+		
+	}
+	public static int Sum(int x, int y){
+		int z = 0;
+		z = x + y;
+		
+		return z;
+	}
+
+}
